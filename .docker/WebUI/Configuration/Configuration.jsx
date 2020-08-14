7ca865e878cbb1738f96d7bd77b26c3322e63cd2
Eric Maino <ericmai@microsoft.com>
2020-08-14 13:08:32 -0700

    Basic CSE Config

--- .docker/WebUI/Configuration/Configuration.jsx
+++ .docker/WebUI/Configuration/Configuration.jsx
@@ -1,4 +1,4 @@
-﻿    import WorkItemDetails from "../parts.optional/WorkItemDetails/WorkItemDetails";
+﻿import WorkItemDetails from "../parts.optional/WorkItemDetails/WorkItemDetails";
 
     export default {
         // Arriba Service URL to query
@@ -8,64 +8,72 @@
              (window.location.port > 0 ? ":" + window.location.port : "" ) + 
              "/v1",
 
-        // Name of tool to show [top right and elsewhere]
-        toolName: "CSEng",
+    // Name of tool to show [top right and elsewhere]
+    toolName: "CSEng",
 
-        // E-Mail addresses to whom feedback should go
-        feedbackEmailAddresses: "slouvau@hotmail.com",
+    // E-Mail addresses to whom feedback should go
+    feedbackEmailAddresses: "slouvau@hotmail.com",
 
-        // Listing: Default Columns and Sort Order to show per table
-        listingDefaults: {
-             "CSEng": { columns: ["ID", "Title", "Assigned To"], sortColumn: "ID", sortOrder: "desc" }
+    // Listing: Default Columns and Sort Order to show per table
+    listingDefaults: {
+        "Organizations": { columns: ["ID", "Title", "Region", "CountrySelection", "CSEIndustry"], sortColumn: "ID", sortOrder: "desc" },
+        "Engagements": { columns: ["ID", "Work Item Type", "Title", "State", "Assigned To"], sortColumn: "ID", sortOrder: "desc" },
+        "Activities": { columns: ["ID", "Work Item Type", "Title", "State", "Assigned To"], sortColumn: "ID", sortOrder: "desc" },
+        "ResourceRequests": { columns: ["ID", "Work Item Type", "Title", "State", "Assigned To"], sortColumn: "ID", sortOrder: "desc" },
+        "EngineeringFeedback": { columns: ["ID", "Work Item Type", "Title", "State", "Assigned To"], sortColumn: "ID", sortOrder: "desc" },
+    },
+
+    // Listing: Default details rendering per table
+    customDetailsProviders: {
+        "Organizations": WorkItemDetails,
+        "Engagements": WorkItemDetails,
+        "Activities": WorkItemDetails,
+        "ResourceRequests": WorkItemDetails,
+        "EngineeringFeedback": WorkItemDetails,
+    },
+
+    databaseName: "CSEng",
+    directLinkUrl: "https://dev.azure.com/csefy19/_workitems/edit/",
+
+    // Grid: Pre-defined Grid queries which appear in the 'Load' dropdown in the Grid view
+    /* gridDefault: "<NameOfDefaultPredefinedQuery>", */
+    gridDefaultQueries: {
+        "Clear": {},
+        "Arriba Tasks By Owner": {
+            query: "Arriba AND [Remaining Work] > 0",
+            aggregationFunction: "SUM",
+            aggregateColumn: "Remaining Work",
+            currentTable: "Louvau",
+            rows: ["[Assigned To]>"],
+            showPortionOf: "row"
         },
+    },
 
-        // Listing: Default details rendering per table
-        customDetailsProviders: {
-            "CSEng": WorkItemDetails
-        },
-
-        databaseName : "CSEng",
-        directLinkUrl : "https://dev.azure.com/csefy19/_workitems/edit/",
-
-        // Grid: Pre-defined Grid queries which appear in the 'Load' dropdown in the Grid view
-        /* gridDefault: "<NameOfDefaultPredefinedQuery>", */
-        gridDefaultQueries: {
-            "Clear": {},
-            "Arriba Tasks By Owner": {
-                query: "Arriba AND [Remaining Work] > 0",
-                aggregationFunction: "SUM",
-                aggregateColumn: "Remaining Work",
-                currentTable: "Louvau",
-                rows: ["[Assigned To]>"],
-                showPortionOf: "row"
-            },
-        },
-
-        // Content to show before a query is typed
-        startContent: {
-            overview:(
-                <span>
-                    All Louvau work items indexed.
-                    Need more <a href="Search.html?help=true">help</a>?
-                </span>
-            ),
-            examples: {
-                "[Area Path] : Arriba AND [Priority] <= 1 AND [State] != Closed": <span>Arriba Active</span>,
-                "Arriba AND [Remaining Work] > 0": <span>Arriba Remaining Tasks</span>
-            }
-        },
-
-        // Content to show if query fails due to access denied (401)
-        accessDeniedContent: (
+    // Content to show before a query is typed
+    startContent: {
+        overview: (
             <span>
-                Access to CSEng denied.
+                All items indexed.
+                    Need more <a href="Search.html?help=true">help</a>?
             </span>
         ),
+        examples: {
+            "[Area Path] : Arriba AND [Priority] <= 1 AND [State] != Closed": <span>Arriba Active</span>,
+            "Arriba AND [Remaining Work] > 0": <span>Arriba Remaining Tasks</span>
+        }
+    },
 
-        // Content to show if query fails because service is unavailable (404, 500)
-        serviceUnavailableContent: (
-            <span>
-                CSEng is currently unavailable.
-            </span>
-        )
-    };
+    // Content to show if query fails due to access denied (401)
+    accessDeniedContent: (
+        <span>
+            Access to CSEng denied.
+        </span>
+    ),
+
+    // Content to show if query fails because service is unavailable (404, 500)
+    serviceUnavailableContent: (
+        <span>
+            CSEng is currently unavailable.
+        </span>
+    )
+};
